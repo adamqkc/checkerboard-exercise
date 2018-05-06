643bd1edf31da820a30820b6e108b41da7469779
ADAM CHEN <ADAMQKC@GMAIL.COM>
2018-05-06 02:23:56 -0700

    delete this

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// Your JS goes here
+//function to create tile
+function tiles(color) {
+  let tile = document.createElement('div');
+
+  tile.style.width = '11.1%';
+  tile.style.float = 'left';
+  tile.style.paddingBottom = '11.1%';
+  tile.style.background = color;
+
+  return tile;
+}
+
+// function to create random color
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+// function to create checkerboard
+function checkerboard(size) {
+  let body = document.querySelector('body');
+
+  for (let i = 0; i < size; i++) {
+    body.appendChild(tiles(getRandomColor()));
+  }
+}
+
+checkerboard(81);
