28bf8a84cabdbbe3995c4dfebea2b90daba032ac
ADAM CHEN <ADAMQKC@GMAIL.COM>
2018-05-05 21:54:20 -0700

    add random color feature

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// Your JS goes here
+//BRANCH: randomcolors
+
+//function to create tile
+function tiles(color) {
+  let tile = document.createElement('div');
+
+  tile.style.width = '11.1%';
+  tile.style.float = 'left';
+  tile.style.paddingBottom = '11.1%';
+  tile.style.background = color;
+
+  return tile;
+}
+
+//function to create checkerboard
+function checkerboard(size) {
+  let body = document.querySelector('body');
+
+  for (let i = 0; i < size; i++) {
+    if (i % 2 === 0) {
+      body.appendChild(tiles('black'))
+    } else {
+      body.appendChild(tiles('red'));
+    }
+  }
+}
+
+checkerboard(81);
