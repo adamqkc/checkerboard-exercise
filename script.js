075f39b1851eb2cf90a2aa8087cf8f1f9a1a1389
ADAM CHEN <ADAMQKC@GMAIL.COM>
2018-05-05 22:56:40 -0700

    add randomcolors branch

--- script.js
+++ script.js
@@ -12,16 +12,23 @@ function tiles(color) {
   return tile;
 }
 
+//function to create random color
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
 //function to create checkerboard
 function checkerboard(size) {
   let body = document.querySelector('body');
 
   for (let i = 0; i < size; i++) {
-    if (i % 2 === 0) {
-      body.appendChild(tiles('black'))
-    } else {
-      body.appendChild(tiles('red'));
-    }
+    body.appendChild(tiles(getRandomColor()));
   }
 }
 
