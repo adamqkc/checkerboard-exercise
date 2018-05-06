56a66e251ec9e50e302262c1cce217d4a2d5770d
ADAM CHEN <ADAMQKC@GMAIL.COM>
2018-05-05 23:23:45 -0700

    add randomcolors

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//BRANCH: randomcolors
-
 //function to create tile
 function tiles(color) {
   let tile = document.createElement('div');
@@ -12,24 +10,40 @@ function tiles(color) {
   return tile;
 }
 
-//function to create random color
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-
 //function to create checkerboard
 function checkerboard(size) {
   let body = document.querySelector('body');
 
   for (let i = 0; i < size; i++) {
-    body.appendChild(tiles(getRandomColor()));
+    if (i % 2 === 0) {
+      body.appendChild(tiles('black'))
+    } else {
+      body.appendChild(tiles('red'));
+    }
   }
 }
 
 checkerboard(81);
+
+//function to create random color
+// function getRandomColor() {
+//   var letters = '0123456789ABCDEF';
+//   var color = '#';
+//
+//   for (var i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//
+//   return color;
+// }
+//
+//function to create checkerboard
+// function checkerboard(size) {
+//   let body = document.querySelector('body');
+//
+//   for (let i = 0; i < size; i++) {
+//     body.appendChild(tiles(getRandomColor()));
+//   }
+// }
+//
+// checkerboard(81);
