64f77dec45a52cad8f053c9396dd52ac74015521
ADAM CHEN <ADAMQKC@GMAIL.COM>
2018-05-05 23:28:24 -0700

    add random colors feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//function to create tile
+// function to create tile
 function tiles(color) {
   let tile = document.createElement('div');
 
@@ -10,40 +10,25 @@ function tiles(color) {
   return tile;
 }
 
-//function to create checkerboard
+// function to create random color
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+// function to create checkerboard
 function checkerboard(size) {
   let body = document.querySelector('body');
 
   for (let i = 0; i < size; i++) {
-    if (i % 2 === 0) {
-      body.appendChild(tiles('black'))
-    } else {
-      body.appendChild(tiles('red'));
-    }
+    body.appendChild(tiles(getRandomColor()));
   }
 }
 
 checkerboard(81);
-
-//function to create random color
-// function getRandomColor() {
-//   var letters = '0123456789ABCDEF';
-//   var color = '#';
-//
-//   for (var i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//
-//   return color;
-// }
-//
-//function to create checkerboard
-// function checkerboard(size) {
-//   let body = document.querySelector('body');
-//
-//   for (let i = 0; i < size; i++) {
-//     body.appendChild(tiles(getRandomColor()));
-//   }
-// }
-//
-// checkerboard(81);
